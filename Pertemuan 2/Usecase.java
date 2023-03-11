8c3ba963ab22df1b9c06525bcad8e5e578b19cba
Fazar Budiman <fazarbudiman1907@gmail.com>
2023-03-11 15:19:04 +0700

    upload pertemuan 2

---
+++ Pertemuan 2/Usecase.java
@@ -0,0 +1,29 @@
+class students {
+    int id;
+    String name;
+    String grade;
+}
+
+
+class videos {
+    int id;
+    String title;
+    String descVideo;
+    String urlVideo;
+    session session;
+}
+
+
+class pengajar {
+    int id;
+    String name;
+    String grade;
+    videos video;
+}
+
+
+class session {
+    int room;
+    String name;
+    String grade;
+}
